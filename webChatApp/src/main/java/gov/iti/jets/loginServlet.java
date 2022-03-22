803c106372e9165fe769ebb254c71d9c38c19542
Marwa Adel <marwaayosiif@gmail.com>
2022-03-22 11:42:14 +0200

    LoginServlet

---
+++ webChatApp/src/main/java/gov/iti/jets/loginServlet.java
@@ -0,0 +1,24 @@
+package gov.iti.jets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class loginServlet extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        String name = request.getParameter("name");
+        String gender = request.getParameter("gender");
+        System.out.println(name+ " "+gender);
+        session.setAttribute("name", name);
+        session.setAttribute("gender", gender);
+        response.sendRedirect("");
+    }
+}
